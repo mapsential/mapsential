c0922ab47d4bee4e619178a2cebbb54161b42a8d
Flasderstein <clemenssz@gmx.de>
2021-10-21 18:00:41 +0200

    Add Dependencies to Package.json

--- mapsential-frontend/src/index.js
+++ mapsential-frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MapsentialMain from "./mapsentialMain/MapsentialMain";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MapsentialMain />
   </React.StrictMode>,
   document.getElementById('root')
 );
