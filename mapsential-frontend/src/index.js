8fe7fb8879d8c4d82d2739615cba80ca4a20c897
0scarB <oscarb@protonmail.com>
2021-11-02 08:04:11 +0100

    Serve frontend with nginx

--- mapsential-frontend/src/index.js
+++ mapsential-frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import MapsentialMain from "./mapsentialMain/MapsentialMain";
