9a391348ce216648ac51fd64970a839ad44aff72
0scarB <oscarb@protonmail.com>
2022-02-11 14:53:46 +0100

    Add routes based on current location

--- mapsential-frontend/src/index.js
+++ mapsential-frontend/src/index.js
@@ -9,7 +9,7 @@ import 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
 
 ReactDOM.render(
     <React.StrictMode>
